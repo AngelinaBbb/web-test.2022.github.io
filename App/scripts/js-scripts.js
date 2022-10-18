df59f5ecf66716ad3e0cecbbd16db74cb89dfc6e
AngelinaBbb <116116723+AngelinaBbb@users.noreply.github.com>
2022-10-18 22:53:51 +0300

    Add files via upload

---
+++ App/scripts/js-scripts.js
@@ -0,0 +1,8 @@
+ function superClick(el) {
+    el.style.background = '#E0FFFF';
+    el.style.color = 'black';
+        }
+function changeDisplay() {
+     const header3 = document.getElementById("1");
+        header3.style.display= 'yellow';
+        }
